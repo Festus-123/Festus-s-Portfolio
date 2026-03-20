0219bd02cb57e0452a97d1188a6669900e3bad6d
Festus Phillip <st.festus4cruise@gmail.com>
2026-03-20 16:15:58 +0100

    adding final touches

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY || "");
-console.log(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 if(!process.env.ESEND_API_KEY) {
     throw new Error("Missing Resend API Key")
