c0874296e1bb69a6852a0b00201e3013de8cfeac
Festus Phillip <st.festus4cruise@gmail.com>
2026-03-20 16:18:06 +0100

    new

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,9 +2,9 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-if(!process.env.ESEND_API_KEY) {
-    throw new Error("Missing Resend API Key")
-}
+// if(!process.env.ESEND_API_KEY) {
+//     throw new Error("Missing Resend API Key")
+// }
 
 export async function POST(req: Request) {
   const { email, name, message } = await req.json();
